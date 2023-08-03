22365db197a6f459e07307991417a3a9caee454c
Steve Pflug <stephen@pflug.us>
2023-08-03 08:40:39 -0500

    first commit and one on one with rick

---
+++ src/rickDiaz/rickDiaz.java
@@ -0,0 +1,16 @@
+package rickDiaz;
+
+public class rickDiaz {
+    public static void main(String[] args) {
+        String myString = "H";
+        String yourString = "H";
+        char myLetter = 'J';
+        int myNumber = 6;
+
+        myString = "    Hello, World!     ";
+
+        System.out.println(myString.trim());
+
+        System.out.println(myString);
+    }
+}
